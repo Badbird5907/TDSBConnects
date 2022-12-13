23fd566094868f4879b443cf31f51a9ee032d64a
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2022-12-13 00:21:29 -0500

    Make the ui render correctly (remove react native default bloat)

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import {
-    SafeAreaView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-} from 'react-native';
-
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import Home from './pages/Home';
+import HomeScreen from './pages/Home';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-    const isDarkMode = useColorScheme() === 'dark';
-
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    };
-
     return (
-        <SafeAreaView style={backgroundStyle}>
-            <StatusBar
-                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-                backgroundColor={backgroundStyle.backgroundColor}
-            />
-            <Text
-                style={[
-                    styles.sectionTitle,
-                    {
-                        color: isDarkMode ? Colors.white : Colors.black,
-                    },
-                ]}>
-                hi 1
-            </Text>
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen
-                        name="Home"
-                        component={Home}
-                        options={{title: 'Welcome'}}
-                    />
-                    {/*
-                    <Stack.Screen
-                        name="Profile"
-                        component={ProfileScreen}
-                    />
-                    */}
-                </Stack.Navigator>
-            </NavigationContainer>
-        </SafeAreaView>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen}/>
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 };
 
-const styles = StyleSheet.create({
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-    },
-    highlight: {
-        fontWeight: '700',
-    },
-});
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,10 @@
 import * as React from 'react';
-import {StyleSheet, Text, useColorScheme} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {Text, View} from 'react-native';
 
-const Home = () => {
-    const isDarkMode = useColorScheme() === 'dark';
+export default function HomeScreen() {
     return (
-        <Text
-            style={[
-                styles.sectionTitle,
-                {
-                    color: isDarkMode ? Colors.white : Colors.black,
-                },
-            ]}>
-            hi
-        </Text>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style={{color: 'black'}}>Home Screen</Text>
+        </View>
     );
-};
-const styles = StyleSheet.create({
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-    },
-    highlight: {
-        fontWeight: '700',
-    },
-});
-export default Home;
+}

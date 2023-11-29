cd20e211d3d514e447e321ad6d1be08ffe79dcd2
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-11-29 17:04:28 -0500

    more bootstrap stuff

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
     <View className="dark flex-1 items-center justify-center bg-gray-900">
-      <Text className={"text-white"}>Hi :)</Text>
       <StatusBar style="auto" />
     </View>
   );
--- src/Test.tsx
+++ src/Test.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Test = () => {
-  return (
-    <div>
-
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Test;
